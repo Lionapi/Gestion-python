f66ccdaf6c067c7bea5a32a1b5ce5153ce59db9a
Lionapi <ntotcholionel0@gmail.com>
2021-03-16 12:18:11 +0100

    Update Products_tkinter.py

--- gestionpy/gestionpy/Products_tkinter.py
+++ gestionpy/gestionpy/Products_tkinter.py
@@ -5,7 +5,7 @@ import tkinter.ttk
 import pymysql
 
 def DBconnection():
-    con, h, u, p, bd = "", "localhost", "Franck-Lionel", "Franck-Lionel007", "appgestcom"
+    con, h, u, p, bd = "", "localhost", "user", "password", "appgestcom"
 
     try:
         con = pymysql.connect(host = h, user = u, password = p, db = bd)
